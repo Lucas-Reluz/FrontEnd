63f23b0c297b15001ffb79bf57071b7aad6becc7
Lucas-Reluz <lucas.pokemon.61@gmail.com>
2022-05-24 23:14:13 -0300

    @Lucas-Reluz [Task: Criado pasta para JS e modificado modal e botão fechar no css] {24/05/2022 | 23:14}

---
+++ Assets/js/script.js
@@ -0,0 +1,19 @@
+function acao(){
+    let modal = document.querySelector('.modal')
+
+    modal.style.display = 'block';
+}
+
+function fechar(){
+    let modal = document.querySelector('.modal')
+
+    modal.style.display = 'none';
+}
+
+/* 
+por tag: getElementByTagName()
+por Id: getElementById()
+por Nome: getElementByName()
+por Classe: getElementByClassName()
+por Seletor: querySelector()
+*/
