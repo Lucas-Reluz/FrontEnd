d9e6476c55b6100affb881f695d08fe853bf19c8
Lucas-Reluz <lucas.pokemon.61@gmail.com>
2022-06-19 16:12:47 -0300

    @Lucas-Reluz [Task: Modificando pasta V2] {16:12 | 19/06/2022}

---
+++ Front-Pokemon/Assets/js/script.js
@@ -0,0 +1,44 @@
+let nome = window.document.getElementById('nome')
+let senha = window.document.getElementById('senha')
+
+function acao(){
+    let modal = document.querySelector('.modal')
+
+    modal.style.display = 'block';
+}
+
+function fechar(){
+    let modal = document.querySelector('.modal')
+
+    modal.style.display = 'none';
+}
+
+
+/*function validaNome() {
+    let txt = document.querySelector('#txtnome')
+    
+    if (nome.value.length < 3){
+        txt.innerHTML = 'Nome Inválido'
+        txt.style.color = 'red'
+    }else{
+        txt.innerHTML = ''
+    }
+}
+
+function validaSenha(){
+    let txtsenha = document.querySelector('#txtsenha')
+    if (senha.value.length < 7){
+        txtsenha.innerHTML = 'Senha Curta'
+        txtsenha.style.color = 'red'
+    }
+    else{
+     txtsenha.innerHTML = ''
+    }
+}*/
+/* 
+por tag: getElementByTagName()
+por Id: getElementById()
+por Nome: getElementByName()
+por Classe: getElementByClassName()
+por Seletor: querySelector()
+*/
